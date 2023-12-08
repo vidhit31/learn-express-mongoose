f5180e7dc5f59d85c705805768ce677cb449ac67
Vidhi Thacker <46989276+vidhit31@users.noreply.github.com>
2023-12-08 00:06:44 -0500

    Update server2.js

--- basics/server2.js
+++ basics/server2.js
@@ -13,10 +13,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req, res) => {
-  return res.send({ping: 'pong'});
-});
-
 app.use(function (req, res, next) {
   console.log('hello');
   next(new NotFoundError());
@@ -25,7 +21,12 @@ app.use(function (req, res, next) {
 app.use(function(err, req, res, next) {
   const status = err.status || 500;
   const message = err.message;
-  return res.status(status).json({
-    error: {message, status}
-  });
+  // return res.status(status).json({
+  //   error: {message, status}
+  // });
+  next();
+});
+
+app.get('/', (req, res) => {
+  return res.send({ping: 'pong'});
 });
